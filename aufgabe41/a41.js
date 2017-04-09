ac37864e70cde1f58a9515b0ae02a4ca57b71298
sara <sara@192.168.2.105>
2017-04-09 14:37:57 +0200

    Aufgabe 4.1

---
+++ aufgabe41/a41.js
@@ -0,0 +1,52 @@
+let sum = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    let rice = 1;
+    let line = 0;
+    for (let n = 0; n < 64; n++) {
+        let element = document.createElement("div");
+        if (((n + line) % 2) == 0) {
+            element.className = "board black";
+        }
+        else {
+            element.className = "board white";
+        }
+        if (((n + 1) % 8) == 0) {
+            line++;
+        }
+        element.innerText = "" + rice;
+        rice *= 2;
+        document.body.appendChild(element);
+        if (n < 8) {
+            element.addEventListener("click", mark);
+        }
+    }
+    function mark(event) {
+        let target = event.target;
+        if (target.className.includes("marked")) {
+            if (target.className.includes("black")) {
+                target.className = "board black";
+            }
+            else {
+                target.className = "board white";
+            }
+        }
+        else {
+            target.className += " marked";
+        }
+        sum = 0;
+        let marked = document.getElementsByClassName("marked");
+        for (let i = 0; i < marked.length; i++) {
+            sum += parseInt(marked[i].innerText);
+        }
+        console.log(" Summe Reiskörner: " + "\r\n" + "Dezimal: " + sum.toString() + "\r\n" + "Hexadezimal: " + sum.toString(16));
+    }
+    let mouseBox = document.createElement("div");
+    document.body.appendChild(mouseBox);
+    mouseBox.className = "mouseBox";
+    mouseBox.addEventListener("mousemove", function (Event) {
+        mouseBox.style.left = (Event.clientX + 10) + "px";
+        mouseBox.style.top = (Event.clientY + 10) + "px";
+    });
+});
+//# sourceMappingURL=a41.js.map 
+//# sourceMappingURL=a41.js.map
