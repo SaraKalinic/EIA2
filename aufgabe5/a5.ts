1f618e8cb99874a0363e6b1592edeabf39d304c6
sara <sara@192.168.2.102>
2017-04-20 18:05:09 +0200

    Aufgabe 4

--- aufgabe5/a5.ts
+++ aufgabe5/a5.ts
@@ -46,11 +46,12 @@ namespace L4_Canvas {
 
         //Wiesen
         drawWiese3(0, 473, "#2d8659", "#2d8659");
+        drawFluss(1100, 473, "#004d00", "#99e6ff", " #cceeff", "#cceeff");
         drawWiese1(1920, 473, "#339966", "#339966");
         drawWiese2(0, 540, "#adebad", "#adebad");
 
         //Haus
-        drawHaus(0, 540, "#734d26", "#d9b38c", "#cc9966", "#cc9966");
+        drawHaus(0, 540, "#734d26", "#d9b38c", "#663300", "#cc9966");
         drawTure(0, 540, "#4d2600", "#ac7339");
         drawFenster(0, 540, "#4da6ff", "#cce6ff");
         drawFenster(165, 540, "#4da6ff", "#cce6ff");
@@ -68,7 +69,7 @@ namespace L4_Canvas {
         //Blume zufällig platzieren
         for (var i: number = 0; i < 19; i++) {
             let blumenFeld: number = Math.floor((Math.random() * 4) - 1);
-            let _x: number = Math.floor(Math.random() * (1620 - 140)) + 140;
+            let _x: number = Math.floor(Math.random() * (1620 - 180)) + 180;
             let _y: number = Math.floor(Math.random() * (1000 - 850)) + 850;
 
             switch (blumenFeld) {
@@ -615,6 +616,67 @@ namespace L4_Canvas {
         crc2.stroke();
 
 
+    }
+    
+    function drawFluss(_x: number, _y: number, _strokeColor: string, _fillColor: string, _strokeColor1: string, _fillColor1: string): void {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.moveTo(_x + 50, _y -2);
+        crc2.lineTo(_x + 10 , _y + 50);
+        crc2.lineTo(_x  , _y + 60);
+        crc2.lineTo(_x - 8  , _y + 65);
+        crc2.lineTo(_x - 20  , _y + 70);
+        crc2.lineTo(_x - 30  , _y + 80);
+        crc2.lineTo(_x - 40  , _y + 89);
+        crc2.lineTo(_x - 50  , _y + 95);
+        crc2.lineTo(_x - 60  , _y + 105);
+        crc2.lineTo(_x - 110  , _y + 135);
+        crc2.lineTo(_x - 200  , _y + 165); 
+        crc2.lineTo(_x - 290  , _y + 185); 
+        crc2.lineTo(_x - 340  , _y + 195);
+        crc2.lineTo(_x - 360  , _y + 210);
+        crc2.lineTo(_x - 400  , _y + 230);
+        crc2.lineTo(_x - 550  , _y + 200);
+        crc2.lineTo(_x - 410  , _y + 140);
+        crc2.lineTo(_x - 260  , _y + 110);
+        crc2.lineTo(_x - 180  , _y + 90);
+        crc2.lineTo(_x - 150  , _y + 80);
+        crc2.lineTo(_x - 80  , _y + 50);
+        crc2.lineTo(_x - 50  , _y + 30);
+        crc2.lineTo(_x +10, _y );
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+        
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor1;
+        crc2.strokeStyle = _strokeColor1;
+        crc2.moveTo(_x + 45, _y +3);
+        crc2.lineTo(_x + 30 , _y + 3);
+        crc2.moveTo(_x - 15 , _y +15);
+        crc2.lineTo(_x +5 , _y + 15);
+        crc2.moveTo(_x +24 , _y +30);
+        crc2.lineTo(_x - 10 , _y + 30);
+        crc2.moveTo(_x -59 , _y +40);
+        crc2.lineTo(_x -25 , _y + 45);
+        crc2.moveTo(_x -25 , _y +70);
+        crc2.lineTo(_x -50 , _y + 68);
+        crc2.moveTo(_x -120 , _y +70);
+        crc2.lineTo(_x -80 , _y + 80);
+        crc2.moveTo(_x -69 , _y +110);
+        crc2.lineTo(_x -100 , _y + 104);
+        crc2.moveTo(_x -205 , _y +100);
+        crc2.lineTo(_x -145 , _y + 115);
+        crc2.moveTo(_x -165 , _y +150);
+        crc2.lineTo(_x -210 , _y + 140);
+        crc2.moveTo(_x -349 , _y +130);
+        crc2.lineTo(_x -300 , _y + 150);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+        
+        
     }
 
 }
