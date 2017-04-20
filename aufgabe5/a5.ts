ff64fc4d73a9b0b395521e0ee7a764a56198dba0
sara <sara@192.168.2.102>
2017-04-20 13:56:28 +0200

    Aufgabe 5

--- aufgabe5/a5.ts
+++ aufgabe5/a5.ts
@@ -50,23 +50,24 @@ namespace L4_Canvas {
         drawWiese2(0, 540, "#adebad", "#adebad");
 
         //Haus
-        drawHaus(0, 540, "#734d26", "#d9b38c");
+        drawHaus(0, 540, "#734d26", "#d9b38c", "#cc9966", "#cc9966");
         drawTure(0, 540, "#4d2600", "#ac7339");
         drawFenster(0, 540, "#4da6ff", "#cce6ff");
         drawFenster(165, 540, "#4da6ff", "#cce6ff");
         drawBank(20, 700, "#734d26", "#d9b38c", "#4d2600", "#4d2600");
         drawStraße(0, 540, "#4d4d4d", "#999999");
-
-      /*  //Blumen
+        drawBusch( 0, 540, 100, 180, 270, "#339966", "#339966");
+        drawBusch1( 190, 540, 100, 180, 270, "#339966", "#339966");
+      /* //Blumen
         drawBlume(800, 800, 5, 180, 270, "#ffff99", "#ffff99", "#4d2600", "#4d2600");
         drawBlume1(900, 800, 5, 180, 270, "#ffffff", "#ffffff", "#ffff1a", "#ffff1a");
         //drawBlume2(500, 800, 5, 180, 270, "#ff80df", "#ff80df", "#ff80df", "#ff80df");
         drawBlume3(500, 900, 5, 180, 270, "#66ccff", "#66ccff", "#3399ff", "#3399ff", "#ffff00", "#ffff00", "#663300", "#663300");
 */
-        
+
         //Blume zufällig platzieren
         for (var i: number = 0; i < 19; i++) {
-            let blumenFeld: number = Math.floor((Math.random() * 4) -1);
+            let blumenFeld: number = Math.floor((Math.random() * 4) - 1);
             let _x: number = Math.floor(Math.random() * (1620 - 140)) + 140;
             let _y: number = Math.floor(Math.random() * (1000 - 850)) + 850;
 
@@ -188,14 +189,14 @@ namespace L4_Canvas {
         crc2.stroke();
     }
 
-      function drawBerg1(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
+    function drawBerg1(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.strokeStyle = _strokeColor;
-        crc2.moveTo(_x +30 , _y );
-        crc2.lineTo(_x - 180 , _y -300 );
-        crc2.lineTo(_x - 360, _y );
-        crc2.lineTo(_x , _y  );
+        crc2.moveTo(_x + 30, _y);
+        crc2.lineTo(_x - 180, _y - 300);
+        crc2.lineTo(_x - 360, _y);
+        crc2.lineTo(_x, _y);
         crc2.closePath();
         crc2.fill();
         crc2.stroke();
@@ -215,12 +216,23 @@ namespace L4_Canvas {
     }
 
     // Haus
-    function drawHaus(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
+    function drawHaus(_x: number, _y: number, _strokeColor: string, _fillColor: string, _strokeColor1: string, _fillColor1: string): void {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.strokeStyle = _strokeColor;
         crc2.moveTo(_x, _y);
         crc2.lineTo(_x + 300, _y);
+        crc2.lineTo(_x + 300, _y - 200);
+        crc2.lineTo(_x + 150, _y - 300);
+        crc2.lineTo(_x, _y - 200);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor1;
+        crc2.strokeStyle = _strokeColor1;
+        crc2.moveTo(_x, _y - 200);
         crc2.lineTo(_x + 300, _y - 200);
         crc2.lineTo(_x + 150, _y - 300);
         crc2.lineTo(_x, _y - 200);
@@ -276,7 +288,97 @@ namespace L4_Canvas {
         crc2.fill();
         crc2.stroke();
     }
+    
+      function drawBusch(_x: number, _y: number, _x1: number, _y1: number, r: number, _strokeColor: string, _fillColor: string): void {
+          crc2.beginPath();
+          crc2.fillStyle = _fillColor;
+          crc2.strokeStyle = _strokeColor;
+          crc2.moveTo(_x, _y);
+          crc2.arc(_x, _y, 15, 180, 270);
+          crc2.moveTo(_x, _y);
+          crc2.arc(_x + 10, _y - 10, 15, 180, 270);
+          crc2.moveTo(_x, _y);
+          crc2.arc(_x + 20, _y - 10, 15, 180, 270);
+          crc2.moveTo(_x, _y);
+          crc2.arc(_x + 30, _y, 15, 180, 270);
+          crc2.moveTo(_x, _y);
+          crc2.arc(_x + 20, _y + 5, 15, 180, 270);
+          crc2.closePath();
+          crc2.fill();
+          crc2.stroke();
+          
+          crc2.beginPath();
+          crc2.fillStyle = _fillColor;
+          crc2.strokeStyle = _strokeColor;
+          crc2.moveTo(_x + 40, _y + 25);
+          crc2.arc(_x + 40, _y + 25, 15, 180, 270);
+          crc2.moveTo(_x + 50, _y + 15);
+          crc2.arc(_x + 50, _y + 15, 15, 180, 270);
+          crc2.moveTo(_x + 65, _y + 15);
+          crc2.arc(_x + 65, _y + 15, 15, 180, 270);
+          crc2.moveTo(_x + 75, _y + 25);
+          crc2.arc(_x + 75, _y + 25, 15, 180, 270);
+          crc2.moveTo(_x + 55, _y + 25);
+          crc2.arc(_x + 55, _y + 25, 15, 180, 270);
+          crc2.closePath();
+          crc2.fill();
+          crc2.stroke();
 
+          
+          crc2.beginPath();
+          crc2.fillStyle = _fillColor;
+          crc2.strokeStyle = _strokeColor;
+          crc2.moveTo(_x + 80, _y);
+          crc2.arc(_x + 80, _y, 15, 180, 270);
+          crc2.moveTo(_x + 90, _y);
+          crc2.arc(_x + 90, _y - 8, 15, 180, 270);
+          crc2.moveTo(_x + 100, _y);
+          crc2.arc(_x + 100, _y - 8, 15, 180, 270);
+          crc2.moveTo(_x + 110, _y);
+          crc2.arc(_x + 110, _y, 15, 180, 270);
+          crc2.moveTo(_x + 100, _y);
+          crc2.arc(_x + 100, _y + 5, 15, 180, 270);
+          crc2.closePath();
+          crc2.fill();
+          crc2.stroke(); 
+    }
+
+    
+    function drawBusch1(_x: number, _y: number, _x1: number, _y1: number, r: number, _strokeColor: string, _fillColor: string): void {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.moveTo(_x, _y);
+        crc2.arc(_x, _y, 15, 180, 270);
+        crc2.moveTo(_x, _y);
+        crc2.arc(_x + 10, _y - 10, 15, 180, 270);
+        crc2.moveTo(_x, _y);
+        crc2.arc(_x + 20, _y - 10, 15, 180, 270);
+        crc2.moveTo(_x, _y);
+        crc2.arc(_x + 30, _y, 15, 180, 270);
+        crc2.moveTo(_x, _y);
+        crc2.arc(_x + 20, _y + 5, 15, 180, 270);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+          
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.moveTo(_x + 70, _y);
+        crc2.arc(_x + 70, _y, 15, 180, 270);
+        crc2.moveTo(_x + 80, _y);
+        crc2.arc(_x + 80, _y - 10, 15, 180, 270);
+        crc2.moveTo(_x + 90, _y);
+        crc2.arc(_x + 90, _y - 10, 15, 180, 270);
+        crc2.moveTo(_x + 100, _y);
+        crc2.arc(_x + 100, _y, 15, 180, 270);
+        crc2.moveTo(_x + 90, _y);
+        crc2.arc(_x + 90, _y + 5, 15, 180, 270);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke(); 
+    }
     //Sonnenblume
     function drawBlume(_x: number, _y: number, _x1: number, _y1: number, r: number, _strokeColor: string, _fillColor: string, _fillColor1: string, _strokeColor1: string): void {
         crc2.beginPath();
