760addf895f211c11ad52fb1c95b25b8989087a9
sara <sara@192.168.2.100>
2017-07-18 01:45:53 +0200

    + Gebot 2

--- semesteraufgabe/ameise2.js
+++ semesteraufgabe/ameise2.js
@@ -1,3 +1,10 @@
+/*Aufgabe: (Semesteraufgabe)
+Name: (Sara Kalinic)
+Matrikel: (255073)
+Datum: (16.07.2017)
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert. */
 var Sem;
 (function (Sem) {
     class AmeiseRot extends Sem.Ameise {
